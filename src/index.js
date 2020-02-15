95d067c2e8e4f1dd2613b750cc9acd57c62e215d
DikijBoris <nemiga911@yandex.ru>
2020-02-15 13:47:14 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '../dist/index.html';
+import '../dist/assets/styles/styles.css';
+
+
+console.log('started');
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: ['webpack/hot/dev-server', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader',
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+};
