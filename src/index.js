d303f2af0050c63856538312dc6a48c8f1427008
DikijBoris <nemiga911@yandex.ru>
2020-02-16 08:18:55 +0300

    slider commented out

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '../dist/index.html';
 import '../dist/assets/styles/styles.css';
-import Carousel from './components/homePageComponents/carousel.js';
+// import Carousel from './components/homePageComponents/carousel.js';
 
 console.log('started');
-const carousel = new Carousel();
+// const carousel = new Carousel();
